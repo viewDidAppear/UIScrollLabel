8f115d49db599fd3d17df84336bc1f054d6bb2a3
topLayoutGuide <cocotutch@Rosario.local>
2017-11-11 22:19:25 +1100

    Initial Commit

---
+++ UIScrollLabel/UIScrollLabel.h
@@ -0,0 +1,19 @@
+//
+//  UIScrollLabel.h
+//  UIScrollLabel
+//
+//  Created by Ben Deckys on 11/11/17.
+//  Copyright © 2017 MIMIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIScrollLabel.
+FOUNDATION_EXPORT double UIScrollLabelVersionNumber;
+
+//! Project version string for UIScrollLabel.
+FOUNDATION_EXPORT const unsigned char UIScrollLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIScrollLabel/PublicHeader.h>
+
+
